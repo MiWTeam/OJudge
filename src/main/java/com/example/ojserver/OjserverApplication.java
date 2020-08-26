452773d32ef0c4c63233db2a7b2321d582db7d52
zsy <592323523@qq.com>
2020-08-26 13:42:27 +0800

    init

---
+++ src/main/java/com/example/ojserver/OjserverApplication.java
@@ -0,0 +1,14 @@
+package com.example.ojserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjserverApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/ojserver/OjserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ojserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
